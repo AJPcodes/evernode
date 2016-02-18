d1ad30ed3d3142603e8afab61bd306bdee22031b
AJP <allenatmc@gmail.com>
2016-02-18 09:51:18 -0600

    setup express server

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Evernone serving on port ${PORT}`);
+});
