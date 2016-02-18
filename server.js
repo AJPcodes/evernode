bb8d48ea4cdcc6c1da34ea73eb309e28818df7f9
AJP <allenatmc@gmail.com>
2016-02-18 10:22:33 -0600

    add Jade view engine to serve index.jade

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 "use strict";
 
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.get('/', (req, res) => {
-  res.send('Server Running');
+  res.render('index');
 });
 
 app.listen(PORT, () => {
