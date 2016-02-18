c38f7e11df85a98ee9af82835a221e8755c156b3
AJP <allenatmc@gmail.com>
2016-02-18 10:36:44 -0600

    add path and form for not entry

--- server.js
+++ server.js
@@ -11,8 +11,19 @@ const PORT = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/notes/new', (req, res) => {
+  res.render('new-note');
+});
+
+app.post('/notes', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
